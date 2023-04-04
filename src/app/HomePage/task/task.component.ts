6627815e66ca1f16977abf4bc20f97e5162afc72
Diana Levchenko <dianka_levchenko@inbox.ru>
2023-04-04 15:05:02 +0200

    Delete task request to api handled.

--- src/app/HomePage/task/task.component.ts
+++ src/app/HomePage/task/task.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { TaskModel } from 'src/app/api-models/task.model';
+import { environment } from 'src/app/environments/environment';
 
 @Component({
   selector: 'app-task',
@@ -8,5 +10,12 @@ import { TaskModel } from 'src/app/api-models/task.model';
 })
 export class TaskComponent {
   @Input() task: TaskModel;
+  constructor(private http: HttpClient){
+
+  }
   
+  onDeleteTask(taskId: number){
+    this.http.delete(`${environment.apiUrl}TaskItem/${taskId}`)
+    .subscribe();
+  }
 }
