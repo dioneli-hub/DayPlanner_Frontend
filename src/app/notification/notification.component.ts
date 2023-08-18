bf290ffce9506e145299aa65f9bf0b4f4f849548
Diana Levchenko <dianka_levchenko@inbox.ru>
2023-08-18 19:49:42 +0200

    Notification component styles improved

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 import { Subject } from 'rxjs';
 import { NotificationModel } from 'src/api-models/notification.model';
 
@@ -15,6 +17,8 @@ export class NotificationComponent {
   @Input() notification: NotificationModel | null = null;
   @Output() deleteNotification = new EventEmitter<NotificationModel>();
 
+  constructor(private sanitizer: DomSanitizer){}
+
   ngOnInit() {
     this.daysAgo = this.setDaysAgo();
   }
@@ -22,6 +26,10 @@ export class NotificationComponent {
     this.deleteNotification.emit(notification);
   }
 
+ processNotificationText() : SafeHtml{
+    const processedText = this.notification.text.replace(/\"(.*?)\"/g, '<b>"$1"</b>');
+    return this.sanitizer.bypassSecurityTrustHtml(processedText);
+    }
 
 
   setDaysAgo(){
